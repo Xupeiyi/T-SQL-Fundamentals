6c8a92cbbc36bd22fcc1f94098408d3dfe4cd79f
px14 <px14@duke.edu>
2023-06-13 22:26:45 -0400

    finished 5.1

--- chapter5/exercise1.sql
+++ chapter5/exercise1.sql
@@ -6,3 +6,12 @@ SELECT orderid, orderdate, custid, empid, endofyear
 FROM (SELECT orderid, orderdate, custid, empid, DATEFROMPARTS(YEAR(orderdate), 12, 31) AS endofyear 
 	  FROM Sales.Orders) AS D
 WHERE orderdate <> endofyear;
+
+WITH D AS
+(
+	SELECT orderid, orderdate, custid, empid, DATEFROMPARTS(YEAR(orderdate), 12, 31) AS endofyear 
+	FROM Sales.Orders
+)
+SELECT orderid, orderdate, custid, empid, endofyear
+FROM D
+WHERE orderdate <> endofyear;
